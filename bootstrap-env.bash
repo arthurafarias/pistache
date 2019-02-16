ee619320a9e46a68fdf288c985a63a58f5cea0cc
Arthur de Araújo Farias <arthur@afarias.org>
2019-02-17 00:31:06 +0100

    Testing in travis if this project build accordingly

--- bootstrap-env.bash
+++ bootstrap-env.bash
@@ -1,24 +1,16 @@
 #!/bin/bash -ve
 
-cat << EOF > /etc/apt/sources.list.d/llvm.list
-deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty main
-deb-src http://apt.llvm.org/trusty/ llvm-toolchain-trusty main
-# 7 
-deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-7 main
-deb-src http://apt.llvm.org/trusty/ llvm-toolchain-trusty-7 main
-# 8 
-deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-8 main
-deb-src http://apt.llvm.org/trusty/ llvm-toolchain-trusty-8 main
-
-# Also add the following for the appropriate libstdc++
-# LLVM toolchain trusts ubuntu-toolchain-r/test as libstdc++ reference
-# Trusty repository is broken as this open issue shows https://github.com/stan-dev/math/issues/604
-deb http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu trusty main
-EOF
-
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key| sudo apt-key add -
+export $(cat /etc/*release | grep DISTRIB_CODENAME)
 
 sudo apt-get update
+
+if [[ "$DISTRIB_CODENAME" = "trusty" ]]; then
+    sudo apt-get install -y software-properties-common
+    # LLVM Recomends this PPA so we can trust since ubuntu 14.04 libstdc++
+    # and compilers are broken https://github.com/stan-dev/math/issues/604
+    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+    sudo apt-get update
+fi
 
 if [[ "$COMPILER_NAME" = "clang" ]]; then
     export C_COMPILER_PACKAGE=clang-$COMPILER_VERSION
@@ -26,10 +18,12 @@ if [[ "$COMPILER_NAME" = "clang" ]]; then
     export CPP_COMPILER_PACKAGE=
     export CPP_COMPILER_BIN=clang++-$COMPILER_VERSION
     export COV_TOOL_BIN=llvm-cov-$COMPILER_VERSION
-    if [ ! -z "$(sudo apt-cache search llvm-$COMPILER_VERSION-tools)" ]; then export COV_TOOL_PACKAGE=$COV_TOOL_PACKAGE llvm-$COMPILER_VERSION-tools; fi
-    if [ ! -z "$(sudo apt-cache search llvm-$COMPILER_VERSION)" ]; then export COV_TOOL_PACKAGE=$COV_TOOL_PACKAGE llvm-$COMPILER_VERSION-tools; fi
+    if [ ! -z "$(sudo apt-cache search llvm-$COMPILER_VERSION-tools)" ]; then export COV_TOOL_PACKAGE="$COV_TOOL_PACKAGE llvm-$COMPILER_VERSION-tools"; fi
+    if [ ! -z "$(sudo apt-cache search llvm-$COMPILER_VERSION)" ]; then export COV_TOOL_PACKAGE="$COV_TOOL_PACKAGE llvm-$COMPILER_VERSION-tools"; fi
     export COV_TOOL_ARGS=gcov
-    export CPP_STDLIB_PACKAGE=
+    # This updates libstdc++ to version 4.9 which makes this software compiles accordingly
+    # that is, in trusty this library will just works with g++ 4.9 backport and so on
+    export CPP_STDLIB_PACKAGE=g++-4.9
 elif [[ "$COMPILER_NAME" = "gcc" ]]; then
     export C_COMPILER_PACKAGE=gcc-$COMPILER_VERSION
     export C_COMPILER_BIN=gcc-$COMPILER_VERSION
---
+++ bootstrap-test.bash
@@ -0,0 +1,3 @@
+export COMPILER_NAME=gcc COMPILER_VERSION=4.9
+. ./boolstra-env.bash
+./bootstrap.bash
